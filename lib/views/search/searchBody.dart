edd6d54e79c91278a5bfd686518725c73ec5ee06
ThaysMrtns <thays.martins@nave.org.br>
2021-05-13 15:32:43 -0300

    lista de lugares

--- lib/views/search/searchBody.dart
+++ lib/views/search/searchBody.dart
@@ -9,11 +9,36 @@ class _SearchBodyState extends State<SearchBody> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: GestureDetector(
-        onTap: () {
-          Navigator.of(context).pop();
-        },
-        child: Text("VOLTAR"),
+      margin: EdgeInsets.only(top: 20, bottom: 40),
+      padding: EdgeInsets.all(20),
+      child: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: ListView(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(bottom: 40),
+                  color: Colors.blueGrey,
+                  width: MediaQuery.of(context).size.width * .2,
+                  height: MediaQuery.of(context).size.height * .2,
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 40),
+                  color: Colors.blue,
+                  width: MediaQuery.of(context).size.width * .2,
+                  height: MediaQuery.of(context).size.height * .2,
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 40),
+                  color: Colors.blueGrey,
+                  width: MediaQuery.of(context).size.width * .2,
+                  height: MediaQuery.of(context).size.height * .2,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
