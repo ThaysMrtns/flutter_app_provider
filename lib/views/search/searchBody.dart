49e51cac11b8ea14e1345fe53091dce558c7db4f
ThaysMrtns <thays.martins@nave.org.br>
2021-05-13 15:43:08 -0300

    card container

--- lib/views/search/searchBody.dart
+++ lib/views/search/searchBody.dart
@@ -10,6 +10,17 @@ class _SearchBodyState extends State<SearchBody> {
   Widget build(BuildContext context) {
     double widthCard = MediaQuery.of(context).size.width * .2;
     double heightCard = MediaQuery.of(context).size.height * .2;
+    EdgeInsets marginCard = EdgeInsets.only(bottom: 40);
+
+    Container card() {
+      return Container(
+        margin: marginCard,
+        color: Colors.blueGrey,
+        width: widthCard,
+        height: heightCard,
+      );
+    }
+
     return Container(
       margin: EdgeInsets.only(top: 20, bottom: 40),
       padding: EdgeInsets.all(20),
@@ -19,24 +30,9 @@ class _SearchBodyState extends State<SearchBody> {
             flex: 1,
             child: ListView(
               children: [
-                Container(
-                  margin: EdgeInsets.only(bottom: 40),
-                  color: Colors.blueGrey,
-                  width: widthCard,
-                  height: heightCard,
-                ),
-                Container(
-                  margin: EdgeInsets.only(bottom: 40),
-                  color: Colors.blue,
-                  width: widthCard,
-                  height: heightCard,
-                ),
-                Container(
-                  margin: EdgeInsets.only(bottom: 40),
-                  color: Colors.blueGrey,
-                  width: widthCard,
-                  height: heightCard,
-                ),
+                card(),
+                card(),
+                card(),
               ],
             ),
           ),
