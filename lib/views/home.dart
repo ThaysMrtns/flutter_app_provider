c688edc17cabda64ef75f15b6404a99125b46cc4
ThaysMrtns <thays.martins@nave.org.br>
2021-05-18 15:15:06 -0300

    apagando arquivos desnecessarios

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'login/login.dart';
 import '../main.dart';
-import 'package:provider/provider.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -16,27 +14,26 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: hexToColor("#FFFFFF"),
-        title: Text(
-          "Explore",
-          style: TextStyle(
-            color: Colors.black,
+          backgroundColor: hexToColor("#FFFFFF"),
+          title: Text(
+            "Explore",
+            style: TextStyle(
+              color: Colors.black,
+            ),
           ),
-        ),
-        toolbarHeight: 60,
-        centerTitle: true,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.person, color: Colors.amber),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => Login()),
-              );
-            },
-          ),
-        ]
-      ),
+          toolbarHeight: 60,
+          centerTitle: true,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.person, color: Colors.amber),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Login()),
+                );
+              },
+            ),
+          ]),
       body: HomeBody(),
     );
   }
@@ -45,7 +42,6 @@ class _HomePageState extends State<HomePage> {
     return new Color(
         int.parse(hexCode.substring(1, 7), radix: 16) + 0xFF000000);
   }
-  
 }
 
 class HomeBody extends StatefulWidget {
@@ -64,29 +60,18 @@ class _HomeBodyState extends State<HomeBody> {
 
   @override
   Widget build(BuildContext context) {
-    double heightCard = MediaQuery.of(context).size.height * .12;
-    EdgeInsets marginCard = EdgeInsets.only(bottom: 40, left: 30, right: 30);
-    
     return Container(
-      margin: EdgeInsets.only(top: 20, bottom: 40),
-      padding: EdgeInsets.all(20),
-      child: Column(
-        children: [ 
-          Consumer<SignInModel>(
-            builder: (context, userModel, child){
-              String message = (userModel.user == ""? "Por favor, faça login.": "Olá, ${userModel.user}.");
+        margin: EdgeInsets.only(top: 20, bottom: 40),
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Consumer<SignInModel>(builder: (context, userModel, child) {
+              String message = (userModel.user == ""
+                  ? "Por favor, faça login."
+                  : "Olá, ${userModel.user}.");
               return Text(message);
-            }
-          ),
-        ],)
-    );
+            }),
+          ],
+        ));
   }
-  
 }
-
-  
-
-
-
-
-
